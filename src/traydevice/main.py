67d40af07153024ad894f0b02f3779a65f88e683
shpelda <shpelda@seznam.cz>
2009-10-19 02:14:34 +0200

    corrected copyright notice

--- src/traydevice/main.py
+++ src/traydevice/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#Copyright (C) 2007  Martin Špelina <shpelda at seznam dot cz>
+#Copyright (C) 2009  Martin Špelina <shpelda at seznam dot cz>
 #
 #This program is free software; you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by

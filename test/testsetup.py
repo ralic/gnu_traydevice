55fdb2b124685836deb049ab60c2367e7292b22a
shpelda <shpelda@gmail.com>
2012-10-07 22:19:54 +0200

    tests are not part of binary distribution

--- test/testsetup.py
+++ test/testsetup.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 from unittest import main
 
-import setup
-
 
 class test_cut_prefix(TestCase):
     def test_prefix_equals_pathname(self):
@@ -12,4 +10,10 @@ class test_cut_prefix(TestCase):
 
 
 if __name__=='__main__':
+    import os,sys,inspect
+    cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+    setup_folder = os.path.abspath('%s/..'%cmd_folder)
+    if setup_folder not in sys.path:
+        sys.path.insert(0, setup_folder)
+    import setup
     main()    
